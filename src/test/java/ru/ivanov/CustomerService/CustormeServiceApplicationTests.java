487955ed0890e96f88818bb6bdf1ea973bd19325
Viktor4010 <avacode78@gmail.com>
2023-03-04 10:32:56 +0700

    initial commit. Connect to database

---
+++ src/test/java/ru/ivanov/CustomerService/CustormeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ivanov.CustomerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustormeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
