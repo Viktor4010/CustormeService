81ae33ae7b56aa7ddc93865d77640805f03a2421
Viktor4010 <avacode78@gmail.com>
2023-03-07 11:43:13 +0700

    add basic logger

--- src/main/java/ru/ivanov/CustomerService/CustomerServiceApplication.java
+++ src/main/java/ru/ivanov/CustomerService/CustomerServiceApplication.java
@@ -9,13 +9,13 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CustomerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
 
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
--- src/main/java/ru/ivanov/CustomerService/services/AddressService.java
+++ src/main/java/ru/ivanov/CustomerService/services/AddressService.java
@@ -1,5 +1,7 @@
 package ru.ivanov.CustomerService.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,7 +10,7 @@ import ru.ivanov.CustomerService.repositories.AddressRepository;
 
 @Service
 public class AddressService {
-
+    private static final Logger log = LoggerFactory.getLogger(AddressService.class);
     private final AddressRepository addressRepository;
 
     @Autowired
@@ -23,7 +25,7 @@ public class AddressService {
 
         updatedAddress.setId(id);
         updatedAddress.setClient(addressToBeUpdated.getClient());
-
+        log.info("Update user address");
         addressRepository.save(updatedAddress);
     }
 }
